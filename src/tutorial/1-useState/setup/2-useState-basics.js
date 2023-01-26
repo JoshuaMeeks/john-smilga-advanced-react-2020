3c62beb9fe5b3c6f28bbf726deab55cdfd403a04
Your Name <jlmeeks587@gmail.com>
2023-01-26 15:06:15 -0500

    State management section 1

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random title')
+
+  const handleClick = () => {
+    return (text === 'random title' ? setText('hello world') : setText('random title'))
+  }
+  return (
+    <>
+      <h1>{text}</h1>
+      <button type='button' className='btn' onClick={handleClick}>Change Title</button>
+    </>
+  );
 };
 
 export default UseStateBasics;
