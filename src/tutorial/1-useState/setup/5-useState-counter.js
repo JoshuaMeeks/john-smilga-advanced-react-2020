2771f2eaea355ba31ede4c0c2eb2aa1913c6b153
Your Name <jlmeeks587@gmail.com>
2023-01-27 11:50:08 -0500

    useEffect basics

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
+
   const reset = () => { setValue(0) }
+
+  const complexIncrease = () => {
+    setTimeout(() => {setValue((prevValue) => {return prevValue + 1})}, 2000)
+  }
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -11,6 +16,11 @@ const UseStateCounter = () => {
         <button className='btn' onClick={() => setValue(value - 1)}>decrease</button>
         <button className='btn' onClick={reset}>reset</button>
         <button className='btn' onClick={() => setValue(value + 1)}>increase</button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>increase later</button>
       </section>
     </>
   );
