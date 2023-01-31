4a0a93a8aebe90545bb718dd8fcfea245e73dfef
Your Name <jlmeeks587@gmail.com>
2023-01-31 17:45:06 -0500

    removePerson function added but will not work without prop drilling

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -5,25 +5,36 @@ import { data } from '../../../data';
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  const [people, setPeople] = useState(data)
-  return <section>
-    <h3>prop drilling</h3>
-    <List people={people}/>
-  </section>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((person) => {
+      return people.filter((person) => person.id !== id);
+    })
+  }
+  return (
+    <section>
+      <h3>prop drilling</h3>
+      <List people={people}/>
+    </section>
+  );
 };
 
 const List = ({people}) => {
-  return <>
-    {people.map((person) => {
-      return <SinglePerson key={person.id} />
-    })}
-  </>
+  return (
+    <>
+      {people.map((person) => {
+        return <SinglePerson key={person.id} />;
+      })}
+    </>
+  );
 }
 
 const SinglePerson = ({id, name}) => {
-  return <div className='item'>
-    <h4>single item</h4>
-  </div>
+  return (
+    <div className='item'>
+      <h4>single item</h4>
+    </div>
+  );
 }
 
 export default PropDrilling;
