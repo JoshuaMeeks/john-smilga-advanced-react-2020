577eb71d4d0ddacc8afb70ebfbc79874eb2e95b6
Your Name <jlmeeks587@gmail.com>
2023-02-02 12:53:11 -0500

    calculateMostExpensive function added

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -6,6 +6,15 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  return data.reduce((total, item) => {
+    const price = item.fields.price;
+    if (price >= total) {
+      total = price;
+    }
+    return total;
+  }, 0)/100
+};
 
 const Index = () => {
   const { products } = useFetch(url);
